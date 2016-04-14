a7219c743751059c3083692b94876db74447d979
mwaleedkhan <mwaleedkhan-developer@outlook.com>
2016-04-15 02:27:12 +0500

    Xamarin Installation and Testing the application development enviornment.
    Proposal and Prototype also given.

---
+++ VP Concepts Practice/Basics/String Reverse/String Reverse/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Reverse
+{
+    delegate void del(int a);
+    class Program
+    {
+        public void func(int a)
+        {
+            Console.WriteLine(a * a);
+        }
+        public void func2(int a)
+        {
+            Console.WriteLine(a + a);
+        }
+        static void Main(string[] args)
+        {
+
+            Program obj = new Program();
+            del a = new del(obj.func);
+            a(2);
+            a = new del(obj.func2);
+            a(3);
+            Console.ReadLine();
+
+        }
+    }
+}
