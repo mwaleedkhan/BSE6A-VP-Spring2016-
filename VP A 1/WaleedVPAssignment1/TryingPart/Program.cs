ad8b0c18c221ed705c9bf9cad7e6a9974b287a60
mwaleedkhan <mwaleedkhan-developer@outlook.com>
2016-05-27 13:22:09 +0500

    vp a1

---
+++ VP A 1/WaleedVPAssignment1/TryingPart/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryingPart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myDater = new DateTime();
+            myDater = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine(myDater.Date);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
